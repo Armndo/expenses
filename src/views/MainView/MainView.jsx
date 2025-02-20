4bf6215aedd433cc3da6ebab9a2e4a3869a28fc2
Armando Gonzalez <armndo.g@gmail.com>
2025-02-20 16:26:12 -0600

    feat(mainview): implemented fetching of sources on loading view

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -1,5 +1,45 @@
+import { api_url } from "@/env"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export function MainView({}) {
-  return <div>
-    main view
-  </div>
+  const [state, setState] = useState({
+    sources: [],
+    loading: true,
+  })
+  const navigate = useNavigate()
+
+  function load() {
+    axios.get(
+      `${api_url}/sources`,
+      {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`
+        }
+      }
+    ).then(res => {
+      setState(prev => ({ ...prev, sources: res.data }))
+    }).catch(err => {
+      if (err?.status === 401) {
+        sessionStorage.removeItem("token")
+        navigate("/login")
+      }
+    }).finally(() => setState(prev => ({ ...prev, loading: false })))
+  }
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  return (!state.loading ?
+    <div>
+      <h1>expenses</h1>
+      sources:
+      <ul>
+        {state.sources.map(source => <li key={source.id}>{source.name}</li>)}
+      </ul>
+    </div> :
+    <div>loading</div>
+  )
 }
