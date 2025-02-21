77d72bc52abecce8a8c959280e4474ec2956c6d9
Armando Gonzalez <armndo.g@gmail.com>
2025-02-21 10:48:44 -0600

    feat(mainview): implemented husk functionality to store, update and destroy expenses

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -7,6 +7,7 @@ export function MainView({ }) {
   const [state, setState] = useState({
     sources: [],
     loading: true,
+    editing: null,
   })
   const navigate = useNavigate()
 
@@ -15,7 +16,7 @@ export function MainView({ }) {
       `${api_url}/expenses`,
       {
         headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("token")}`
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
         },
       }
     ).then(res => {
@@ -28,6 +29,75 @@ export function MainView({ }) {
     }).finally(() => setState(prev => ({ ...prev, loading: false })))
   }
 
+  function editExpense(field, value) {
+    const editing = {
+      ...state.editing,
+      [field]: value !== "" ? value : null,
+    }
+
+    setState(prev => ({
+      ...prev,
+      editing,
+    }))
+  }
+
+  function store(index) {
+    const expense = { ...state.editing }
+
+    if (expense.amount === null || isNaN(expense.amount)) {
+      alert("Monto en formato incorrecto.")
+      return
+    }
+
+    expense.amount = +expense.amount
+    const expenses = [ ...state.sources[index].expenses ]
+    expenses.push(expense)
+    const source = { ...state.sources[index], expenses }
+    const sources = [ ...state.sources ]
+    sources[index] = source
+
+    setState(prev => ({
+      ...prev,
+      editing: null,
+      sources
+    }))
+  }
+
+  function update(index, index2) {
+    const expense = { ...state.editing }
+
+    if (expense.amount === null || isNaN(expense.amount)) {
+      alert("Monto en formato incorrecto.")
+      return
+    }
+
+    expense.amount = +expense.amount
+    const expenses = [ ...state.sources[index].expenses ]
+    expenses[index2] = expense
+    const source = { ...state.sources[index], expenses }
+    const sources = [ ...state.sources ]
+    sources[index] = source
+
+    setState(prev => ({
+      ...prev,
+      editing: null,
+      sources
+    }))
+  }
+
+  function destroy(index, index2) {
+    const expenses = [ ...state.sources[index].expenses ]
+    expenses.splice(index2, 1)
+    const source = { ...state.sources[index], expenses }
+    const sources = [ ...state.sources ]
+    sources[index] = source
+
+    setState(prev => ({
+      ...prev,
+      sources
+    }))
+  }
+
   useEffect(() => {
     load()
   }, [])
@@ -36,7 +106,7 @@ export function MainView({ }) {
     <div>
       <h1>expenses</h1>
       <div>
-        {state.sources.map(source => <table key={`source${source.id}`} style={{ display: "inline-block", verticalAlign: "top", textAlign: "center" }} border={1}>
+        {state.sources.map((source, index) => <table key={`source${source.id}`} style={{ display: "inline-block", verticalAlign: "top", textAlign: "center" }} border={1}>
           <thead>
             <tr>
               <th colSpan={4}>{source.name}</th>
@@ -45,23 +115,72 @@ export function MainView({ }) {
               <th>fecha</th>
               <th>monto</th>
               <th>descripción</th>
-              <th>eliminar</th>
+              <th>acción</th>
             </tr>
           </thead>
           <tbody>
-            {source.expenses.map(expense => <tr key={`expense${expense.id}`}>
-              <td>{new Date(expense.date).toLocaleDateString()}</td>
-              <td>${(+expense.amount).toFixed(2)}</td>
-              <td>{expense.description ?? "-"}</td>
-              <td>
-                <button style={{ width: "100%" }}>x</button>
-              </td>
+            {source.expenses.map((expense, index2) => <tr key={`expense${expense.id}`}>
+              {state.editing === null || state.editing?.id !== expense.id ?
+                <>
+                  <td>
+                    {new Date(expense.date).toLocaleDateString()}
+                  </td>
+                  <td>
+                    ${expense.amount.toFixed(2)}
+                  </td>
+                  <td>
+                      {expense.description ?? "-"}
+                  </td>
+                  <td>
+                    <button onClick={() => setState(prev => ({ ...prev, editing: { ...expense, source_id: source.id } }))}>✎</button>
+                    <button onClick={() => destroy(index, index2)}>⌫</button>
+                  </td>
+                </> :
+                state.editing && <>
+                  <td>
+                    <input type="date" value={state.editing?.date} onChange={e => editExpense("date", e.target.value)} />
+                  </td>
+                  <td>
+                    $<input type="number" min={0.01} step={0.01} value={state.editing?.amount} onChange={e => editExpense("amount", e.target.value)} />
+                  </td>
+                  <td>
+                      <textarea rows={1} value={state.editing?.description ?? ""} onChange={e => editExpense("description", e.target.value)} ></textarea>
+                  </td>
+                  <td>
+                    <button onClick={() => update(index, index2)}>✓</button>
+                    <button onClick={() => setState(prev => ({ ...prev, editing: null }))}>x</button>
+                  </td>
+                </>
+              }
             </tr>)}
+            {state.editing?.source_id === source.id && state.editing?.id === undefined && <tr>
+              <td>
+                <input type="date" value={state.editing.date} onChange={e => editExpense("date", e.target.value)} />
+              </td>
+              <td>
+                $ <input type="number" min={0.01} step={0.01} placeholder={"0.01"} value={state.editing.amount ?? ""} onChange={e => editExpense("amount", e.target.value)} />
+              </td>
+              <td>
+                  <textarea rows={1} onChange={e => editExpense("description", e.target.value)} value={state.editing.description ?? ""}></textarea>
+              </td>
+              <td>
+                <button onClick={() => store(index)}>✓</button>
+                <button onClick={() => setState(prev => ({ ...prev, editing: null }))}>x</button>
+              </td>
+            </tr>}
           </tbody>
           <tfoot>
             <tr>
+              <th>
+                total
+              </th>
+              <td colSpan={3}>
+                ${source.expenses.reduce((a, b) => a + b.amount, 0).toFixed(2)}
+              </td>
+            </tr>
+            <tr>
               <td colSpan={4}>
-                <button style={{ width: "100%" }}>+</button>
+                <button style={{ width: "100%" }} onClick={() => setState(prev => ({ ...prev, editing: { date: (new Date()).toISOString().split("T")[0], amount: null, description: null, source_id: source.id } }))}>+</button>
               </td>
             </tr>
           </tfoot>
