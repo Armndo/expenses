ff6c92d385f2b0f5b2cf5d1dc9bc5a4cb606d8c1
Armando Gonzalez <armndo.g@gmail.com>
2025-09-07 23:40:56 -0600

    feat(expensemodal): added more validation and icons to make it look less empty

--- src/components/ExpensesModal/ExpensesModal.jsx
+++ src/components/ExpensesModal/ExpensesModal.jsx
@@ -1,5 +1,6 @@
 import { api_url } from "@/env"
-import { Button, Card, CardActions, CardContent, CardHeader, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
+import { AttachMoneyTwoTone, CalendarMonthTwoTone, CategoryTwoTone, CreditCardTwoTone, EventRepeatTwoTone, TextSnippetTwoTone } from "@mui/icons-material"
+import { Button, Card, CardActions, CardContent, CardHeader, FormControl, InputAdornment, InputLabel, MenuItem, Select, TextField } from "@mui/material"
 import axios from "axios"
 
 export function ExpensesModal({ state, setState }) {
@@ -33,12 +34,18 @@ export function ExpensesModal({ state, setState }) {
   function store() {
     const expense = { ...state.expense }
 
-    if (expense.amount === null || isNaN(expense.amount)) {
-      alert("Monto en formato incorrecto.")
+    if (expense.amount === null || isNaN(expense.amount) || expense.amount < 0.01 ) {
+      alert("Incorrect 'Amount' format.")
+      return
+    }
+
+    if (expense.instalments !== null && (!Number.isInteger(+expense.instalments) || +expense.instalments < 2)) {
+      alert("Incorrect 'Installments' format.")
       return
     }
 
     expense.amount = +expense.amount
+    if (expense.category_id === 0) delete expense.category_id
 
     setState(prev => ({
       ...prev,
@@ -66,12 +73,18 @@ export function ExpensesModal({ state, setState }) {
   function update() {
     const expense = { ...state.expense }
 
-    if (expense.amount === null || isNaN(expense.amount)) {
-      alert("Monto en formato incorrecto.")
+    if (expense.amount === null || isNaN(expense.amount) || expense.amount < 0.01 ) {
+      alert("Incorrect 'Amount' format.")
+      return
+    }
+
+    if (expense.instalments !== null && (!Number.isInteger(+expense.instalments) || +expense.instalments < 2)) {
+      alert("Incorrect 'Installments' format.")
       return
     }
 
     expense.amount = +expense.amount
+    if (expense.category_id === 0) delete expense.category_id
 
     setState(prev => ({
       ...prev,
@@ -131,6 +144,9 @@ export function ExpensesModal({ state, setState }) {
                 value={state.expense?.source_id ?? ""}
                 sx={{ width: "100%", mb: "1rem" }}
                 label="Source"
+                startAdornment={<InputAdornment sx={{ mr: 1 }}>
+                  <CreditCardTwoTone />
+                </InputAdornment>}
               >
                 {state.sources.map(source => <MenuItem value={source.id}>{source.name}</MenuItem>)}
               </Select>
@@ -140,29 +156,50 @@ export function ExpensesModal({ state, setState }) {
               required
               type="date"
               label="Date"
+              id="modal-date"
               value={state.expense?.date ?? ""}
               onChange={e => editExpense("expense", "date", e.target.value)}
+              slotProps={{
+                input: {
+                  endAdornment: <InputAdornment sx={{ ml: 1, cursor: "pointer" }} onClick={() => document.querySelector("#modal-date").showPicker()}>
+                    <CalendarMonthTwoTone/>
+                  </InputAdornment>
+                }
+              }}
             />
             <TextField
               required
               sx={{ width: "100%", mb: "1rem" }}
               label="Amount"
               type="number"
-              min={0.01}
-              step={0.01}
               placeholder={"0.01"}
               value={state.expense?.amount ?? ""}
               onChange={e => editExpense("expense", "amount", e.target.value)}
+              slotProps={{
+                input: {
+                  startAdornment: <InputAdornment sx={{ mr: 1 }}>
+                    <AttachMoneyTwoTone />
+                  </InputAdornment>
+                },
+                htmlInput: {
+                  step: 0.01,
+                  min: 0.01,
+                }
+              }}
             />
             <FormControl fullWidth>
               <InputLabel id="select-category">Category</InputLabel>
               <Select
                 labelId="select-category"
                 onChange={e => selectCategory(+e.target.value)}
-                value={state.expense?.category_id ?? ""}
+                value={state.expense?.category_id ?? 0}
                 sx={{ width: "100%", mb: "1rem" }}
                 label="Category"
+                startAdornment={<InputAdornment sx={{ mr: 1 }}>
+                  <CategoryTwoTone />
+                </InputAdornment>}
               >
+                <MenuItem dense value={0}>Select a category</MenuItem>
                 {state.categories.map(category => <MenuItem dense value={category.id}>{category.alias} {category.name}</MenuItem>)}
               </Select>
             </FormControl>
@@ -173,17 +210,37 @@ export function ExpensesModal({ state, setState }) {
               rows={4}
               value={state.expense?.description ?? ""}
               onChange={e => editExpense("expense", "description", e.target.value)}
+              placeholder="Describe your expense..."
+              slotProps={{
+                input: {
+                  startAdornment: <InputAdornment sx={{ mr: 1 }}>
+                    <TextSnippetTwoTone />
+                  </InputAdornment>,
+                  sx: {
+                    alignItems: "flex-start"
+                  }
+                }
+              }}
             />
             <TextField
               sx={{ width: "100%" }}
               label="Installments"
-              min={2}
-              max={36}
-              step={1}
               placeholder={"2"}
               type="number"
               value={state.expense?.instalments ?? ""}
               onChange={e => editExpense("expense", "instalments", e.target.value)}
+              slotProps={{
+                input: {
+                  startAdornment: <InputAdornment sx={{ mr: 1 }}>
+                    <EventRepeatTwoTone />
+                  </InputAdornment>
+                },
+                htmlInput: {
+                  step: 1,
+                  min: 2,
+                  max: 36,
+                }
+              }}
             />
           </form>
         </CardContent>
